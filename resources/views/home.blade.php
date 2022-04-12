81c448b7b9bab05ab065510f747a186658bf392d
kahfi001 <muhamad.kahfi46@gmail.com>
2022-04-12 22:46:09 +0800

    pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <h1>ini Halaman Home</h1>
+  <h3><?php echo $name; ?></h3>
+</body>
+</html>
