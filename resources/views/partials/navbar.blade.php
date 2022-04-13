660fcb0d05af6c1b9647bd71a14064828959aa46
kahfi001 <muhamad.kahfi46@gmail.com>
2022-04-13 21:44:14 +0800

    menghilangkan home

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 {{-- Navbar --}}
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
   <div class="container">
-    <a class="navbar-brand" href="/home"><img src="img/logo.png" alt="logo"></a>
+    <a class="navbar-brand" href="/"><img src="img/logo.png" alt="logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
