394f10565727f9eab00b4c36ac76c106e6854215
kahfi001 <muhamad.kahfi46@gmail.com>
2022-04-19 20:53:00 +0800

    membuat halaman tentangKami

--- resources/views/tentangkami.blade.php
+++ resources/views/tentangkami.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.main')
 
 @section('container')
-  <h1>Ini Halaman tentang kami</h1>
+<div class="text-center">
+  <h1>About Us</h1>
+  <img src="../img/logo.png" alt="" width="30%">
+  <div class="m-5 p-5">
+    <p>Selamat Datang di Website kami. Berdonor adalah website tentang menyalurkan dan menerima sumbangan darah. Berdonor adalah organisasi Non-Profit. </p>
+    <p>Kami memiliki visi yang “Membawa Manfaat Lebih Dari Yang Anda Bayangkan”.
+      Kami akan selalu memberikan yang terbaik untuk anda dalam hal berdonor darah. Kami akan terus mengembangkan diri untuk terus menjadi yang terbaik. Oleh karena itu, kami akan selalu berusaha untuk memberikan pelayanan yang terbaik.</p>
+  </div>
+    
+</div>
 @endsection
