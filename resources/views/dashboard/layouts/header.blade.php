5cf50f2e27bb5ad72b8ba82d9760223a18daad6a
kahfi001 <muhamad.kahfi46@gmail.com>
2022-05-04 01:15:48 +0800

    dashboard UI

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -7,7 +7,7 @@
     <div class="nav-item text-nowrap">
       <form action="/logout" method="post">
         @csrf
-        <button type="submit" class="nav-link px-3 bg-dark border-0">Logout</button>
+        <button type= "submit" class="nav-link px-3 bg-dark border-0">Logout</button>
       </form>
     </div>    
   </div>
